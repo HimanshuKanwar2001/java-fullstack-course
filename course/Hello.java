9233921cdb653ca3ae5100d369dbebdeee2f9f5a
himanshukanwar2001 <himanshukanwar2001@gmail.com>
2025-06-11 23:22:59 +0530

    datatypes

--- course/Hello.java
+++ course/Hello.java
@@ -1,5 +1,16 @@
 class Hello{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int num= 3;
+        byte by=123;
+        short sh=232;
+        long l=132131l;
+        char c='k';
+        boolean b=true;
+
+        float marks=123.23f;
+        System.out.println(num+" "+by+" "+sh+" "+l+" "+c+" "+b);
+        System.out.println(marks);
+        System.out.println("Hello World! ");
+        System.out.println(3+5);
     }
 }
