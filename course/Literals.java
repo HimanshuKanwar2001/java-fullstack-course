d2ec4fa53b01085aa841451f734d0b84dc9d04a3
Himanshukanwar2001 <himanshukanwar2001@gmail.com>
2025-06-12 16:56:00 +0530

    literals

---
+++ course/Literals.java
@@ -0,0 +1,9 @@
+public class Literals {
+    public static void main(String[] args) {
+        // literals
+
+        int num1 = 0x7E;
+        int num = 1_00_00_00_000;
+        System.out.println("num1 " + num1 + " " + num);
+    }
+}
