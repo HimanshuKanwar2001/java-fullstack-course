ac766b65a6f94f0dd37f67158433920d2a2a9ae3
Himanshukanwar2001 <himanshukanwar2001@gmail.com>
2025-06-12 17:17:05 +0530

    arithematic operation

---
+++ course/ArthematicOperation.java
@@ -0,0 +1,18 @@
+public class ArthematicOperation {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 5;
+
+        // int result = num1 + num2;
+        // int result = num1 - num2;
+        // int result = num1 * num2;
+        // int result = num1 % num2;
+        // int result = num1 / num2;
+        int c = 0;
+        c = c + 1;
+        c += 1;
+        int val = c++;
+        int val1 = ++c;
+        System.out.println(val1);
+    }
+}
