5bb95a289cf680f1194c8c2a1693ef6dca9d4a42
Himanshukanwar2001 <himanshukanwar2001@gmail.com>
2025-06-13 11:41:36 +0530

    stack and heap

--- course/Demo.java
+++ course/Demo.java
@@ -1,9 +1,12 @@
 class Calculator {
 
+    int num = 5;// instance variable
+
     // Method Overloading same name different parameters
 
     public int add(int n1, int n2) {
-        return n1 + n2;
+        System.out.println(num);
+        return n1 + n2;// local variable
     }
 
     public int add(int n1, int n2, int n3) {
@@ -15,9 +18,18 @@ class Calculator {
 public class Demo {
     public static void main(String[] args) {
 
+        int data = 10;
+        // cal is a reference variable
         Calculator cal = new Calculator();
+        Calculator cal1 = new Calculator();
+
         int result = cal.add(12, 12, 12);
+
+        cal.num = 12;
+
         System.out.println((result));
+        System.out.println((cal.num));
+        System.out.println((cal1.num));
 
     }
 }
