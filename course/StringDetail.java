9757b8fc0aee5515f74464af9a9eaed0082586f0
Himanshukanwar2001 <himanshukanwar2001@gmail.com>
2025-06-14 17:36:19 +0530

    strings

---
+++ course/StringDetail.java
@@ -0,0 +1,12 @@
+public class StringDetail {
+    public static void main(String[] args) {
+        String n = new String("Kanwar");
+        String name = "Himanshu";
+
+        System.out.println(name + " " + n);
+
+        System.out.println("hashCode :" + n.hashCode());
+        System.out.println("location at index :" + 2 + ":" + n.charAt(2));
+
+    }
+}
